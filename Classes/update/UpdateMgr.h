5e0bfabddb2f23fbf1134bbcd1d7e6305261aa6d
projectdev0 <thunk333@126.com>
2014-11-25 15:13:12 +0800

    请求接口

--- Classes/update/UpdateMgr.h
+++ Classes/update/UpdateMgr.h
@@ -10,8 +10,12 @@ public:
 	~UpdateMgr();
 public:
 	bool RequestUpdateInfo(const std::string& strVersion,const std::string& strUUID);
+	bool RequestLoveEnd(const std::string& strUUID);
+	bool RequestLoveStart(const std::string& strUUID);
 public:
 	void onUpdateRequestCompleted(cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response);
+	void onLoveEndCompleted(cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response);
+	void onLoveStartCompleted(cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response);
 private:
 	void ProcessUpdateInfo(const std::string& strErrno,const std::string& strVersion,const std::string& strUuid,const std::string& strUrl);
 };
--- Classes/update/UpdateMgr.h.cpp
+++ Classes/update/UpdateMgr.h.cpp
@@ -95,6 +95,150 @@ void UpdateMgr::onUpdateRequestCompleted(cocos2d::network::HttpClient *sender, c
 	}
 }
 
+/*
+爱爱结束接口：
+http://www.nuanai.me/a/loveend.php?uuid=de7cff1c51b3f1d64ccdc609d65c6346
+uuid	用户唯一标示
+
+
+返回值
+{
+"errno"  : "0"/"1"/"-1"
+"win":100,                                        //击败百分比
+
+}
+errmsg :正常/uuid错误/其他错误
+win: 最大值是100， 服务器内部有错误的时候是-1.
+*/
+
+bool UpdateMgr::RequestLoveEnd(const std::string& strUUID)
+{
+	std::string strUrl = "http://www.nuanai.me/a/loveend.php?uuid=";
+	strUrl + strUUID;
+
+	cocos2d::network::HttpRequest* request = new  cocos2d::network::HttpRequest();
+	request->setUrl(strUrl.c_str());
+	request->setRequestType(cocos2d::network::HttpRequest::Type::GET);
+	request->setResponseCallback(this, httpresponse_selector(UpdateMgr::onLoveEndCompleted));
+	request->setTag("GET nuanai end info");
+	cocos2d::network::HttpClient::getInstance()->send(request);
+	request->release();
+	return true;
+}
+
+void UpdateMgr::onLoveEndCompleted(cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response)
+{
+	if (!response) {
+		return;     
+	}
+
+	if (0 != strlen(response->getHttpRequest()->getTag())) {
+
+	}
+
+	long statusCode = response->getResponseCode();
+	if (200 != statusCode || !response->isSucceed()) {
+		return;
+	}
+
+	std::vector<char>* buffer = response->getResponseData();
+	std::string strResponse;
+	for (unsigned int i = 0 ; i < buffer->size() ; i++) {
+		char c = (*buffer)[i];
+		strResponse += c;
+	}
+
+	rapidjson::Document d1;
+	d1.Parse<0>(strResponse.c_str());
+
+	//获取JSon数据
+	std::string strErrno;
+	int percent = 50;
+	const rapidjson::Value& vErrno = d1["errno"];
+	if(vErrno.IsString())
+	{
+		strErrno = vErrno.GetString();
+	}
+
+	const rapidjson::Value& vWin = d1["win"];
+	if (vWin.IsInt())
+	{
+		percent = vWin.GetInt();
+	}
+}
+
+/*
+获取人数接口：
+
+http://www.nuanai.me/a/lovestart.php?uuid=de7cff1c51b3f1d64ccdc609d65c6346
+uuid	用户唯一标示
+
+
+返回值
+{
+"errno"  : "0"/"1"/"-1"
+"now":345,                                        //当前人数
+
+}
+errmsg :正常/uuid错误/其他错误
+*/
+
+bool UpdateMgr::RequestLoveStart(const std::string& strUUID)
+{
+	std::string strUrl = "http://www.nuanai.me/a/lovestart.php?uuid=";
+	strUrl + strUUID;
+
+	cocos2d::network::HttpRequest* request = new  cocos2d::network::HttpRequest();
+	request->setUrl(strUrl.c_str());
+	request->setRequestType(cocos2d::network::HttpRequest::Type::GET);
+	request->setResponseCallback(this, httpresponse_selector(UpdateMgr::onLoveStartCompleted));
+	request->setTag("GET nuanai start info");
+	cocos2d::network::HttpClient::getInstance()->send(request);
+	request->release();
+	return true;
+}
+
+void UpdateMgr::onLoveStartCompleted(cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response)
+{
+	if (!response) {
+		return;     
+	}
+
+	if (0 != strlen(response->getHttpRequest()->getTag())) {
+
+	}
+
+	long statusCode = response->getResponseCode();
+	if (200 != statusCode || !response->isSucceed()) {
+		return;
+	}
+
+	std::vector<char>* buffer = response->getResponseData();
+	std::string strResponse;
+	for (unsigned int i = 0 ; i < buffer->size() ; i++) {
+		char c = (*buffer)[i];
+		strResponse += c;
+	}
+
+	rapidjson::Document d1;
+	d1.Parse<0>(strResponse.c_str());
+
+	//获取JSon数据
+	std::string strErrno;
+	int nowPerson = 50;
+	const rapidjson::Value& vErrno = d1["errno"];
+	if(vErrno.IsString())
+	{
+		strErrno = vErrno.GetString();
+	}
+
+	const rapidjson::Value& vNow = d1["now"];
+	if (vNow.IsInt())
+	{
+		nowPerson = vNow.GetInt();
+	}
+}
+
 void UpdateMgr::ProcessUpdateInfo(const std::string& strErrno,const std::string& strVersion,const std::string& strUuid,const std::string& strUrl)
 {
 
